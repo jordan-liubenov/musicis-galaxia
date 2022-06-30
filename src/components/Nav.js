c1c843c7d44265bea4a6b8e47852e48f396a3e91
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-07-01 01:03:48 +0300

    early stages of Navbar component work

---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import "../styles/Nav.css";
+
+import logoImg from "../img/note.png";
+
+const Nav = (props) => {
+  return (
+    <nav></nav>
+  );
+};
+
+export default Nav;
+
+/*
+
+	return (
+		<header>
+			<nav className="navBar">
+				<ul className="links">
+					<div className="logoDiv">
+						<img className="logo" src={logoImg} width={50} height={70}></img>
+					</div>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/catalog">Catalog</a>
+					</li>
+					<li>
+						<a href="/register">Register</a>
+					</li>
+					<li>
+						<a href="/login">Login</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+
+*/
