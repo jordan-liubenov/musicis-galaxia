8070c8225ca3d1258d712cb9f5030ac37a9f65fb
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-07-03 23:59:23 +0300

    finalized Home component (for now)

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<footer>Copyright © 2022</footer>
+		</div>
+	);
+};
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ const Home = (props) => {
 
 	displayUnderscore(underscore, setUnderscore);
 
+	//TO-DO: possibly abstract some parts of this component by splitting it into 1 or 2 smaller components
+
 	return (
 		<div className="homeContainer">
 			<div className="title">
@@ -18,11 +20,37 @@ const Home = (props) => {
 				)}
 			</div>
 			<div className="welcomeMsg">
-				<p className="">
+				<p>
 					Welcome to Musicis Galaxia, an online marketplace<br></br>
 					where everyone is free to post and browse different<br></br>
 					offers on instruments and musical accessories.
 				</p>
+			</div>
+			<div className="instructContainer">
+				<div className="instructMsg">
+					<p>
+						To get started, simply open the{" "}
+						<a className="catalogRef" href="/catalog">
+							Catalog
+						</a>{" "}
+						to browse<br></br>
+						all posts and see if anything catches your eye.<br></br>
+						<br></br>
+						Alternatively, if you'd like to create a new offer and sell
+						<br></br>
+						one of your pieces of gear, create a free account{" "}
+						<a className="catalogRef" href="/register">
+							Here
+						</a>
+						, and <br></br>
+						share your offer with the rest of the users.
+						<br></br>
+					</p>
+					<br></br>
+				</div>
+			</div>
+			<div className="footer">
+				<footer>Copyright Musicis Galaxia © 2022</footer>
 			</div>
 		</div>
 	);
